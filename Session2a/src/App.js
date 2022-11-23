925cc0157949366bbd63371838a436639b1ad8a3
Mallikarjun K Basavanagowdra <44674549+MallikarjunnKB@users.noreply.github.com>
2022-11-23 17:10:35 +0000

    jsx intro

--- Session2a/src/App.js
+++ Session2a/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './style.css'
 
 // const heading = React.createElement("h1", { id: 'title' }, "Hello World"); //basic react element
 //Element -> Object -> HTML (h1)
@@ -7,13 +8,37 @@ import ReactDOM from 'react-dom/client';
 
 //Functional Component - Composition of React Elements
 //Component - returns some react element
-const HeadComponent = () =>{
-    return React.createElement("h1", { id: 'title' }, "Hello World Component");
-}
+// const HeadComponent = () =>{
+//     return React.createElement("h1", { id: 'title' }, "Hello World Component");
+// }
+
+// const heading = React.createElement("div",{id: "title"}, [
+//     React.createElement("h1", {id: "heading"}, "Namaste React Bootcamp"),
+//     React.createElement("h2", {}, "from zero to hero"),
+//     React.createElement("h3", {}, "in 3 months")
+// ])
+
+const noOfMonths = 3;
 
 //JSX - Javascript XML
-const heading = <h1>Hello World JSX</h1>
+const heading = (<div id="title" className='title-class' tabIndex={1}>
+    <h1>Namaste React Bootcamp</h1>
+    <h2>from zero to hero</h2>
+    <h3>in {noOfMonths} months</h3>
+</div>)
 //JSX -> React.createElement -> Object -> DOM (h1)
 
+const TitleComponent = () => <h1>Namaste React Bootcamp</h1>
+
+
+const HeadingComponent = () => {
+    return (
+        <div id="title" className='title-class' tabIndex={1}>
+           <TitleComponent/>
+            <h2>from zero to hero</h2>
+            <h3>in {noOfMonths} months</h3>
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadComponent/>);
+root.render(<HeadingComponent/>);
