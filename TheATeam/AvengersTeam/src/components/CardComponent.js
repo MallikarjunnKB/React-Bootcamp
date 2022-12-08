203d431edbd55ec73e6797bfc741d360a714efc2
Mallikarjun K Basavanagowdra <44674549+MallikarjunnKB@users.noreply.github.com>
2022-12-08 05:29:43 +0000

    assignment5

--- TheATeam/AvengersTeam/src/components/CardComponent.js
+++ TheATeam/AvengersTeam/src/components/CardComponent.js
@@ -1,12 +1,12 @@
 const CardComponent = (props) => {
-    const { id, imageSrc, name, place, company, designation } = props.data
-    console.log(props.data)
+    const { name, bio, avatar_url,company } = props.data
+   
     return (
         <div className="card">
-            <img src={imageSrc} />
+            <img src={avatar_url} />
             <h2>{name}</h2> 
-            <h3>{company} - {designation}</h3>
-            <h4>{place}</h4>
+            <h3>{company}</h3>
+            <h4>{bio}</h4>
         </div>
     )
 }
